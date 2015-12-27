84a9e19c343b53d6ccba7286b6041155ee09abeb
behzadn <b.najafi87@gmail.com>
2015-12-28 00:08:22 +0100

    dividing the data

--- MainRFile.R
+++ MainRFile.R
@@ -2,13 +2,10 @@ setwd("D:/Google Drive/Coursera MAchine learning/Project-HumanActivityRecognitio
 raw_data<-read.csv("pml-training.csv")
 finalTestData<-read.csv("pml-testing.csv")
 dim(raw_data)
-summary(training)
-summary(testing)
-names(testing)
-dim(testing);
-dim(training);
-table(training$classe)
-library(ggplot2);
+table(raw_data$classe)
+library(caret)
+
+
 modFit<-train(classe ~. ,method="rpart", data=training)
 print(modFit)
 plot(modFit$finalModel,uniform=T,main="classification Tree")
@@ -22,7 +19,6 @@ prediciton_training<-predict(modFit,newdata = training)
 confusionMatrix(prediciton_training,training$classe)
 vowel.train$y<-as.factor(vowel.train$y)
 vowel.test$y<-as.factor(vowel.test$y)
-library(caret)
 training<-vowel.train
 testing<-vowel.test
 
