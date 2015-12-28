7b729f39d8245a5e81f98f6a5218a5331607c1a4
behzadn <b.najafi87@gmail.com>
2015-12-28 01:06:51 +0100

    Modified up to building the model

--- MainRFile.R
+++ MainRFile.R
@@ -1,12 +1,23 @@
 setwd("D:/Google Drive/Coursera MAchine learning/Project-HumanActivityRecognition")
-raw_data<-read.csv("pml-training.csv")
-finalTestData<-read.csv("pml-testing.csv")
+raw_data<-read.table("pml-training.csv",header = TRUE, sep = ",", na.strings = c("NA", "#DIV/0!"))
+finalTestData<-read.table("pml-testing.csv",header = TRUE, sep = ",", na.strings = c("NA", "#DIV/0!"))
+
 dim(raw_data)
 table(raw_data$classe)
+
+is_na_result =is.na(raw_data)
+removedColumns = which(colSums(is_na_result) > 13000)
+clean_data1 = raw_data[, -removedColumns]
+dim(clean_data1)
+clean_data = clean_data1[, -c(1:7)]
 library(caret)
-
-
-modFit<-train(classe ~. ,method="rpart", data=training)
+inTrain = createDataPartition(y=raw_data$classe, p=0.7, list=FALSE)
+training = raw_data[inTrain,]
+testing = raw_data[-inTrain,]
+dim(training);dim(testing);
+library(randomForest)
+set.seed(555)
+modFit<-train(classe ~. ,method="rf", data=training)
 print(modFit)
 plot(modFit$finalModel,uniform=T,main="classification Tree")
 text(modFit$finalModel,use.n = T,all = T,cex=0.8)
